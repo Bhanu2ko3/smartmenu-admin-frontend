c4dac3f2ba656b4131d58951532e002772c80579
Bhanu2k03 <bhanukabandara039@gmail.com>
2025-05-27 02:27:51 +0530

    upt

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,11 +1,4 @@
-// app/layout.js
-import AuthProvider from "../components/AuthProvider";
 import "./globals.css";
-
-export const metadata = {
-  title: "Smart Food Admin",
-  description: "Admin panel for Smart Food",
-};
 
 export default function RootLayout({ children }) {
   return (
